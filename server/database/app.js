620ae46d3dfb80b82ed324441d5d18094a4a918c
SuosSovanrith <suos089@gmail.com>
2026-01-06 09:56:43 -0500

    fix for lint 2

--- server/database/app.js
+++ server/database/app.js
@@ -17,9 +17,10 @@ mongoose.connect("mongodb://mongo_db:27017/", { 'dbName': 'dealershipsDB' });
 var Reviews = require('./review');
 var Dealerships = require('./dealership');
 
+// Populate Reviews
 Reviews.deleteMany({}, function(err) {
     if (!err) {
-        Reviews.insertMany(reviews_data['reviews'], function(err2) {
+        Reviews.insertMany(reviews_data.reviews, function(err2) {
             if (err2) console.log(err2);
         });
     } else {
@@ -27,9 +28,10 @@ Reviews.deleteMany({}, function(err) {
     }
 });
 
+// Populate Dealerships
 Dealerships.deleteMany({}, function(err) {
     if (!err) {
-        Dealerships.insertMany(dealerships_data['dealerships'], function(err2) {
+        Dealerships.insertMany(dealerships_data.dealerships, function(err2) {
             if (err2) console.log(err2);
         });
     } else {
@@ -76,4 +78,60 @@ app.get('/fetchDealers', function(req, res) {
 });
 
 // Express route to fetch dealers by state
-app.get('/fetchDealers/:state
+app.get('/fetchDealers/:state', function(req, res) {
+    Dealerships.find({ state: req.params.state }, function(err, documents) {
+        if (err) {
+            res.status(500).json({ error: 'Error fetching documents' });
+        } else {
+            res.json(documents);
+        }
+    });
+});
+
+// Express route to fetch dealer by id
+app.get('/fetchDealer/:id', function(req, res) {
+    Dealerships.find({ id: req.params.id }, function(err, documents) {
+        if (err) {
+            res.status(500).json({ error: 'Error fetching documents' });
+        } else {
+            res.json(documents);
+        }
+    });
+});
+
+// Express route to insert review
+app.post('/insert_review', bodyParser.raw({ type: '*/*' }), function(req, res) {
+    var data = JSON.parse(req.body.toString());
+    Reviews.find().sort({ id: -1 }).exec(function(err, documents) {
+        if (err) {
+            res.status(500).json({ error: 'Error fetching documents' });
+        } else {
+            var new_id = documents[0].id + 1;
+            var review = new Reviews({
+                id: new_id,
+                name: data['name'],
+                dealership: data['dealership'],
+                review: data['review'],
+                purchase: data['purchase'],
+                purchase_date: data['purchase_date'],
+                car_make: data['car_make'],
+                car_model: data['car_model'],
+                car_year: data['car_year']
+            });
+
+            review.save(function(err2, savedReview) {
+                if (err2) {
+                    console.log(err2);
+                    res.status(500).json({ error: 'Error inserting review' });
+                } else {
+                    res.json(savedReview);
+                }
+            });
+        }
+    });
+});
+
+// Start the Express server
+app.listen(port, function() {
+    console.log('Server is running on http://localhost:' + port);
+});
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -129,6 +128,9 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
 
     return JsonResponse({"status": 403, "message": "Unauthorized"})
