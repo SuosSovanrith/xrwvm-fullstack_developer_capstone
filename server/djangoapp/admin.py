f2941bab91929981053532c3d16e69451220a978
SuosSovanrith <suos089@gmail.com>
2026-01-06 08:03:14 -0500

    add carmake an carmodel django

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline): 
+    model = CarModel
+    extra = 1 
+    fields = ('name', 'type', 'year', 'engine_type', 'seating_capacity')
+    show_change_link = True
 
 # CarModelAdmin class
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_of_origin', 'founded_year')
+    search_fields = ('name', 'country_of_origin')
+    inlines = [CarModelInline]
 
 # CarMakeAdmin class with CarModelInline
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'engine_type', 'seating_capacity')
+    list_filter = ('type', 'year', 'engine_type')
+    search_fields = ('name', 'car_make__name')
 
 # Register models here
+
+# Registering models with their respective admins
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     # path for registration
     path(route='register', view=views.registration, name='registration'),
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
     # path for dealer reviews view
 
     # path for add a review view
